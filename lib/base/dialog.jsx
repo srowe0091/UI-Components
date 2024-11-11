441a253f4479048783b154f04f8181ea0edf6293
srowe0091 <srowe0091@gmail.com>
2024-11-11 00:46:34 -0500

    fixed modal sizing

--- lib/base/dialog.jsx
+++ lib/base/dialog.jsx
@@ -32,7 +32,7 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
       ref={ref}
       className={cn(
         // 'fixed left-[50%] top-[25%] md:top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full',
-        'm-auto fixed w-full md:max-w-lg h-full md:h-auto left-0 top-0 md:top-[10%] right-0 z-50 flex flex-col gap-4 border bg-background p-6 shadow-lg sm:rounded-lg data-[state=open]:animate-scale-fade-in data-[state=closed]:animate-scale-fade-out',
+        'm-auto fixed w-full md:max-w-lg h-full md:h-auto left-0 top-0 md:top-[10%] md:bottom-[10%] right-0 z-50 flex flex-col gap-4 border bg-background p-6 shadow-lg sm:rounded-lg data-[state=open]:animate-scale-fade-in data-[state=closed]:animate-scale-fade-out',
         className
       )}
       {...props}
