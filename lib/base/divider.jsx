03cc177a7733379acba20b82b4145e2cdf3f6474
srowe0091 <srowe0091@gmail.com>
2024-11-13 21:29:55 -0500

    removed spacing on dividers

--- lib/base/divider.jsx
+++ lib/base/divider.jsx
@@ -1,8 +1,6 @@
-import { cn } from '../utils'
-
 const Divider = ({ className }) => {
   return (
-    <div className={cn('py-4', className)}>
+    <div className={className}>
       <div className="border-t" />
     </div>
   )
