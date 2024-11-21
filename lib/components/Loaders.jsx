49549a9fc618493e125b9e2fa073f6a14d1adea0
srowe0091 <srowe0091@gmail.com>
2024-11-20 21:21:14 -0500

    Change loaders

--- lib/components/Loaders.jsx
+++ lib/components/Loaders.jsx
@@ -2,6 +2,13 @@ import { LoadingIcon } from '../icons'
 
 export const ContainerLoader = ({ loading }) =>
   loading && (
+    <div className="center absolute top-0 left-0 right-0 bottom-0 z-50 bg-black/25">
+      <LoadingIcon className="animate-spin duration-500 h-10 w-10" />
+    </div>
+  )
+
+export const PageLoader = ({ loading }) =>
+  loading && (
     <div className="center fixed top-0 left-0 right-0 bottom-0 z-50 bg-black/25">
       <LoadingIcon className="animate-spin duration-500 h-10 w-10" />
     </div>
