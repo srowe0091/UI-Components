0e73cc8ee47b0f007cf68a7c4b1ad625e4b5c0c6
srowe0091 <srowe0091@gmail.com>
2025-06-08 22:37:41 -0400

    enhanced fade behavior

--- lib/components/Fade.jsx
+++ lib/components/Fade.jsx
@@ -1,16 +1,19 @@
 import { motion } from 'motion/react'
 
-export const Fade = ({ children, in: isVisible, disableFade, ...rest }) => {
-  if (!isVisible) return null
-
+export const Fade = ({ children, in: isVisible, disableMountAnimation, unmountAnimation, ...rest }) => {
   return (
-    <motion.div
-      initial={disableFade ? false : { opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
-      {...rest}
-    >
-      {children}
-    </motion.div>
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          animate={{ opacity: 1 }}
+          initial={disableMountAnimation ? false : { opacity: 0 }}
+          exit={unmountAnimation ? { opacity: 0 } : false}
+          transition={{ duration: 0.3 }}
+          {...rest}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
