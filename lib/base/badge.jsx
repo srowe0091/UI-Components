8b93c8cbec46b4756e4bd261582548f7a106e3e8
srowe0091 <srowe0091@gmail.com>
2025-07-16 22:08:25 -0400

    added new variant for badge

--- lib/base/badge.jsx
+++ lib/base/badge.jsx
@@ -10,7 +10,8 @@ const badgeVariants = cva(
         default: 'border-transparent bg-primary text-primary-foreground',
         secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive: 'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'text-foreground'
+        outline: 'text-foreground',
+        warning: 'border-transparent bg-orange-500 text-white hover:bg-orange-500/80'
       }
     },
     defaultVariants: {
